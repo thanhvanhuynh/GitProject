e8e2958576b5b1c6b2defd1d453c1fbd525d3bd1
thanhvanhuynh <nguhanh111@gmail.com>
2022-11-12 09:38:47 +0700

    khai bao va su dung bien

--- source/Basic.js
+++ source/Basic.js
@@ -1,5 +1,30 @@
-function myFunction() {
-  let name = prompt("Hãy nhập tên của bạn ...");
-  alert("Hello " + name);
-  document.write("Hello " + name);
+// 3
+let a = prompt("nhap so a");
+let b = prompt("nhap so b");
+document.write("a :" + a);
+document.write("</br>");
+document.write("b :" + b);
+if (a % b === 0) {
+  alert("a la boi cua b");
+} else {
+  alert("a khong phai la boi cua b");
 }
+
+document.write("</br>");
+let width = 20;
+let height = 10;
+let are = width * height;
+document.write("dien tich hinh chu nhat = " + are);
+document.write("</br>");
+let i = 10;
+let f = 20.5;
+let z = true;
+let s = "Hà Nội";
+
+document.write("i = " + i);
+document.write("<br/>");
+document.write("f = " + f);
+document.write("<br/>");
+document.write("z = " + z);
+document.write("<br/>");
+document.write("s = " + s);
